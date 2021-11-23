ffd209af6e85b01b310af307314aa002d06fef78
Nashid-Alam <nashid17@gmail.com>
2021-11-22 21:35:56 -0500

    code clean up and style

--- blackJack.js
+++ blackJack.js
@@ -2,7 +2,7 @@ let suits = ["&hearts;", "&spades;", "&clubs;", "&diams;"]
 let cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"]
 let deck = []
 let gameOver = false
-let scoreBoard =100
+let scoreBoard = 100
 
 const player1 = {
   name: "player1",
@@ -16,51 +16,51 @@ const computer = {
   isPlaying: false,
   handScore: 0,
 }
-const scoreBoardDisplay =document.querySelector(".money")
+const scoreBoardDisplay = document.querySelector(".money")
 const cashoutButton = document.querySelector(".cashout-Button")
 const hitButton = document.querySelector(".hitButton")
 const standButton = document.querySelector(".standButton")
-const resetButton = document.querySelector(".playagain-Button")
+const resetButton = document.querySelector(".reset-Button")
 
 const playerScoreDisplay = document.querySelector("#player1Score")
 const computerScoreDisplay = document.querySelector("#computerScore")
 const player1resultDisplay = document.querySelector(".player1-result")
 const computerresultDisplay = document.querySelector(".computer-result")
 
+function generateDeck() {
+  for (let i = 0; i < cards.length; i++) {
+    for (let j = 0; j < suits.length; j++) {
+      let value = null
+      if (cards[i] === "A") {
+        value = 11
+      } else if (typeof cards[i] === "string") {
+        value = 10
+      } else {
+        value = cards[i]
+      }
 
-function generateDeck(){
-for (let i = 0; i < cards.length; i++) {
-  for (let j = 0; j < suits.length; j++) {
-    let value = null
-    if (cards[i] === "A") {
-      value = 11
-    } else if (typeof cards[i] === "string") {
-      value = 10
-    } else {
-      value = cards[i]
+      let card = {
+        card: cards[i],
+        suit: suits[j],
+        value: value,
+      }
+      deck.push(card)
     }
-
-    let card = {
-      card: cards[i],
-      suit: suits[j],
-      value: value,
-    }
-    deck.push(card)
   }
 }
-}
 generateDeck()
+
 //shuffle the deck
-function shuffledeck(){
-for (let i = deck.length - 1; i > 0; i--) {
-  let j = Math.floor(Math.random() * i);
-  let temp = deck[i];
-  deck[i] = deck[j];
-  deck[j] = temp;
-}
+function shuffledeck() {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i)
+    let temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
+  }
 }
 shuffledeck()
-console.log(deck)
+
 // Draw card function
 function drawCard() {
   if (!gameOver) {
@@ -69,25 +69,22 @@ function drawCard() {
   }
 }
 
-// Hit function
+function updateScore(player){
+  player.handScore = 100
+
+}
+
 function hitMe(player) {
   if (!player.isPlaying) {
     return
   }
   if (player.handScore >= 21) {
-    return 
+    return
   }
 
   const card = drawCard()
   player.hand.push(card)
-  if(card.card !='A'){
-  player.handScore = player.handScore + card.value}
-  if(card.card ==='A'&& player.handScore<=10){
-  player.handScore = player.handScore + 11
-  } 
-  if(card.card ==='A' && player.handScore > 10){
-    player.handScore = player.handScore + 1
-  }
+  updateScore(player)
 
   const cardLi = document.createElement("li")
   const cardSuitTop = document.createElement("div")
@@ -119,23 +116,58 @@ function hitMe(player) {
 
   const handOl = document.querySelector(`#${player.name}-hand`)
   handOl.append(cardLi)
-  
-  if ((player1.handScore === 21)) {
+
+  if (player1.handScore === 21) {
     scoreBoard = scoreBoard + 100
     scoreBoardDisplay.innerHTML = scoreBoard
     player1resultDisplay.style.opacity = "1"
-    player1resultDisplay.innerHTML =" winner!"
-    gameOver=true
-     }
+    player1resultDisplay.innerHTML = " winner!"
+    gameOver = true
+  }
   if (player1.handScore > 21) {
     console.log("player 1 loses")
     scoreBoard = scoreBoard - 100
     scoreBoardDisplay.innerHTML = scoreBoard
     player1resultDisplay.style.opacity = "1"
-    player1resultDisplay.innerHTML ="Bust!"
-    gameOver=true
-   }
+    player1resultDisplay.innerHTML = "Bust!"
+    gameOver = true
+  }
+}
 
+function computerGoes() {
+  while (computer.handScore < 17) {
+    hitMe(computer)
+    computerScoreDisplay.innerHTML = computer.handScore
+    // settimer before running the loop again
+  }
+
+  if (computer.handScore > 21) {
+    scoreBoard = scoreBoard + 100
+    scoreBoardDisplay.innerHTML = scoreBoard
+    computerresultDisplay.style.opacity = "1"
+    return (computerresultDisplay.innerHTML = "Bust!")
+  }
+
+  if (player1.handScore > computer.handScore) {
+    scoreBoard = scoreBoard + 100
+    scoreBoardDisplay.innerHTML = scoreBoard
+    player1resultDisplay.style.opacity = "1"
+    player1resultDisplay.innerHTML = "Winner!"
+    computerresultDisplay.style.opacity = "1"
+    computerresultDisplay.innerHTML = "Lost!"
+    return
+  }
+
+  if (player1.handScore === computer.handScore) {
+    computerresultDisplay.style.opacity = "1"
+    computerresultDisplay.innerHTML = "its a tie!"
+    return
+  } else {
+    scoreBoard = scoreBoard - 100
+    scoreBoardDisplay.innerHTML = scoreBoard
+    computerresultDisplay.style.opacity = "1"
+    return (computerresultDisplay.innerHTML = "Dealer wins!")
+  }
 }
 
 // Event Litsener Functions
@@ -145,72 +177,35 @@ function hitPlayer(e) {
 }
 
 function standPlayer(e) {
-
- if(gameOver===false){
-  player1.isPlaying = false
-  computer.isPlaying = true
-  
-  computerGoes()
-     
-}
-}
-
-function computerGoes() {
-  while (computer.handScore < 17) {
-    hitMe(computer)
-    computerScoreDisplay.innerHTML = computer.handScore
-    // settimer before running the loop again
+  if (gameOver === false) {
+    player1.isPlaying = false
+    computer.isPlaying = true
+    computerGoes()
   }
-  if (computer.handScore > 21) {
-    scoreBoard = scoreBoard + 100
-    scoreBoardDisplay.innerHTML = scoreBoard
-    computerresultDisplay.style.opacity = "1"
-    return computerresultDisplay.innerHTML ="Bust!"
-    }
-   
- if (player1.handScore > computer.handScore && player1.handScore<=21) {
-  scoreBoard = scoreBoard + 100
-  scoreBoardDisplay.innerHTML = scoreBoard
-  computerresultDisplay.style.opacity = "1"
-  return computerresultDisplay.innerHTML ="Bust!"
-   } 
-   
- if (player1.handScore === computer.handScore) {
-  scoreBoard = scoreBoard - 0
-  scoreBoardDisplay.innerHTML = scoreBoard
-  computerresultDisplay.style.opacity = "1"
-  return computerresultDisplay.innerHTML ="its a tie!"
-   
-   } else {
-    scoreBoard = scoreBoard - 100
-    scoreBoardDisplay.innerHTML = scoreBoard
-    computerresultDisplay.style.opacity = "1"
-    return computerresultDisplay.innerHTML ="Dealer wins"
-   }
-  
 }
 
-
-function playAgain(){
-generateDeck()
-shuffledeck()
-player1.name = "player1"
-player1.hand = []
-player1.isPlaying = true
-player1.handScore = 0
-computer.name = "computer"
-computer.hand = []
-computer.isPlaying = false
-computer.handScore = 0  
-playerScoreDisplay.innerHTML = 0
-computerScoreDisplay.innerHTML = 0
-document.querySelector("#player1-hand").innerHTML= ""
-document.querySelector("#computer-hand").innerHTML= ""
-gameOver=false
+function reset(e) {
+  generateDeck()
+  shuffledeck()
+  player1.hand = []
+  player1.isPlaying = true
+  player1.handScore = 0
+  computer.hand = []
+  computer.isPlaying = false
+  computer.handScore = 0
+  playerScoreDisplay.innerHTML = 0
+  computerScoreDisplay.innerHTML = 0
+  document.querySelector("#player1-hand").innerHTML = ""
+  document.querySelector("#computer-hand").innerHTML = ""
+  player1resultDisplay.innerHTML = ""
+  computerresultDisplay.innerHTML = ""
+  gameOver = false
 }
 
-function cashout(){
-    document.body.innerHTML = scoreBoard
+function cashout(e) {
+  document.body.innerHTML = `cash out value \n $ ${scoreBoard}`
+  document.body.style.color = "white"
+  document.body.style.fontSize = "40px"
 }
 // win conditions
 // if ((player1.handScore = 21)) {
@@ -227,7 +222,7 @@ function cashout(){
 
 // if (player1.handScore > computer.handScore) {
 //   console.log("computer loses and player 1 wins")
-// } 
+// }
 
 // if (player1.handScore = computer.handScore) {
 //   console.log('its a tie!!')
@@ -247,5 +242,5 @@ function cashout(){
 //Event listeners
 hitButton.addEventListener("click", hitPlayer)
 standButton.addEventListener("click", standPlayer)
-resetButton.addEventListener('click', playAgain)
-cashoutButton.addEventListener('click',cashout)
+resetButton.addEventListener("click", reset)
+cashoutButton.addEventListener("click", cashout)
